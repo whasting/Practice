1e28c4f3cc4616334cd4f8644225c7c7ba29eb4d
whasting <hastingswade1@gmail.com>
2017-07-18 17:42:56 -0700

    work on diagonal checking

--- eight_queens/solver.rb
+++ eight_queens/solver.rb
@@ -1,9 +1,11 @@
-require 'board.rb'
+require './board.rb'
 require '../perms.rb'
 
 class Solver
+  attr_accessor :solution
+
   def initialize
-    @board = Board.new
+    @solution = []
   end
 
   def solve
@@ -12,8 +14,10 @@ class Solver
     # Each possible solution is a 2d array containing coords
     # [[0,0], [1,1], [2,2], [3,3], [4,4], [5,5], [6,6], [7,7]]
     possible_solutions.each do |possible_solution|
-
+      @solution << possible_solution if check_solution(possible_solution)
     end
+
+    @solution
   end
 
   def generate_moves
@@ -33,18 +37,56 @@ class Solver
     possible_solutions
   end
 
-  # TODO: Make queen class
-
-  # TODO: Starting from starting_pos, in/decrement row and col by 1
-  # until a boundary is reached. Return false if another Queen is
-  # present
-  def check_diagonals(possible_solution)
-    # first take all coordinates and create a hash
+  def possible_solution_to_hash(possible_solution)
     possible_solution_hash = Hash.new
+
     possible_solution.each do |queen_coords|
       possible_solution_hash[queen_coords] = true
     end
 
-    
+    possible_solution_hash
+  end
+
+  def check_solution(possible_solution)
+    possible_solution_hash = possible_solution_to_hash(possible_solution)
+    p possible_solution_hash
+
+    top_left = Proc.new { |pos| pos[0], pos[1] = pos[0] - 1, pos[1] - 1 }
+    bottom_left = Proc.new { |pos| pos[0], pos[1] = pos[0] + 1, pos[1] - 1 }
+    top_right = Proc.new { |pos| pos[0], pos[1] = pos[0] - 1, pos[1] + 1 }
+    bottom_right = Proc.new { |pos| pos[0], pos[1] = pos[0] + 1, pos[1] + 1 }
+
+    correct_solution = true
+
+    possible_solution_hash.each_key do |k|
+      unless check_diagonals(k, possible_solution_hash, &top_left) &&
+             check_diagonals(k, possible_solution_hash, &bottom_left) &&
+             check_diagonals(k, possible_solution_hash, &top_right) &&
+             check_diagonals(k, possible_solution_hash, &bottom_right)
+
+        correct_solution = false
+        break
+      end
+    end
+
+    correct_solution
+  end
+
+  def check_diagonals(coords, possible_solution_hash, &prc)
+    prc ||= Proc.new { |arr| arr[0], arr[1] = arr[0] + 1, arr[1] + 1 }
+
+    selected_coords = prc.call(coords)
+    correct_solution = true
+
+    while (selected_coords[0] > - 1 && selected_coords[1] > -1) &&
+          (selected_coords[0] < 8 && selected_coords[1] < 8) &&
+          correct_solution
+
+      correct_solution = false if possible_solution_hash[selected_coords]
+
+      selected_coords = prc.call(selected_coords)
+    end
+
+    correct_solution
   end
 end
