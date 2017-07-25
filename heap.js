148d0727edc34b2790c0089c1a434bf6a5e96285
whasting <hastingswade1@gmail.com>
2017-07-25 16:21:03 -0700

    create heap skeleton

--- heap.js
+++ heap.js
@@ -1,13 +1,60 @@
 class Heap {
-  constructor( val, parent, children ) {
+  constructor() {
+    this.nodes = [];
+  }
+
+  peek() {
+
+  }
+
+  insert( node ) {
+
+  }
+
+  // return root
+  extract() {
+    return this.nodes[0];
+  }
+
+  deleteRoot() {
+
+  }
+
+  replace( newNode ) {
+
+  }
+
+  createHeap() {
+    return [];
+  }
+
+  heapify() {
+
+  }
+
+  merge( heapOne, heapTwo ) {
+
+  }
+
+  // Like merge!( h1, h2 )
+  meld( heapOne, heapTwo ) {
+
+  }
+
+  size() {
+    return this.nodes.length;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+}
+
+class Node {
+  constructor( val, parent, leftChild, rightChild ) {
     this.value = val;
     this.parent = parent;
-    this.children = children;
+    this.leftChild = leftChild;
+    this.rightChild = rightChild;
   }
-
-  insert() {
-
-  }
-
-  
 }
